6be7fdb96a32677a2f9d5937257e86c685d69351
MaysenS11 <maysen.stiller@htk.academy>
2026-06-10 01:18:48 +0200

    houses

---
+++ Assets/Scripts/Map/Houses.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Houses : MonoBehaviour
+{
+    [SerializeField] private string houseName;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
